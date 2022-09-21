e9a8c762bec663c07a76c8c18d4f7bd595ff9b2a
vijay sb <vijaysb0022@gmail.com>
2022-09-21 17:34:47 +0530

    updated

--- index.js
+++ index.js
@@ -10,53 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-const volunteer=[
-  {
-   "name": "ajay",
-   "email": "ajay@gmail.com",
-   "number": 1234567890,
-   "city": "hyderabad",
-   "state": "telangana",
-   "pincode": 123456,
-   "id": "1"
-  },
-  {
-   "name": "ajay 1",
-   "email": "ajay1@email.com",
-   "number": 1223456789,
-   "city": "hyderabad 1",
-   "state": "telangana",
-   "pincode": 123456,
-   "id": "2"
-  },
-  {
-   "name": "ajay 3",
-   "email": "ajay3@email.com",
-   "number": 1234567893,
-   "city": "hyderabad 3",
-   "state": "telangana 3",
-   "pincode": 123456,
-   "id": "3"
-  },
-  {
-   "name": "ajay 4",
-   "email": "ajay4@email.com",
-   "number": 1234567846,
-   "city": "hyderabad 4",
-   "state": "telangana4",
-   "pincode": 123456,
-   "id": "4"
-  },
-  {
-   "name": "ajay 5",
-   "email": "ajay5@email.com",
-   "number": 1234567868,
-   "city": "hyderabad5",
-   "state": "telangana 5",
-   "pincode": 612347,
-   "id": "5"
-  }
- ]
+
 app.use(cors());
 app.use(express.json());
 
@@ -74,9 +28,6 @@ export const client = await createConnection();
 app.get("/", function (request, response) {
   response.send(" Hi This Is My Trust-project");
 });
-app.get("/volunteer", function (request, response) {
-  response.send(volunteer);
-})
 
 app.use("/users", usersRouter);
 app.use("/reports", reportRouter);
